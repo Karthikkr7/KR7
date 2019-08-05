fa00c9d96dc04bb1e54bdd1d95d82ae9ae26f277
Karthikkr7 <53594349+Karthikkr7@users.noreply.github.com>
2019-08-05 16:32:28 +0530

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,6 +1,6 @@
 a=str(input())
 b=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","p","q","r","s","t","u","v","w","x","y","z"]
 if a in b:
-   print("Constant")
+   print("Consonant")
 else:
   print("invalid") 
