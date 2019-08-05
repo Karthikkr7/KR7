2ad469c80524b97fe0f0507bf5d36b4719a39ca1
Karthikkr7 <53594349+Karthikkr7@users.noreply.github.com>
2019-08-05 16:44:29 +0530

    Update and rename constant.py to vowel.py

--- constant.py
+++
@@ -1,6 +0,0 @@
-a=str(input())
-b=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","p","q","r","s","t","u","v","w","x","y","z"]
-if a in b:
-   print("Consonant")
-else:
-  print("invalid") 
---
+++ vowel.py
@@ -0,0 +1,9 @@
+a=str(input())
+b=["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+c=["a","e","i","o","u"]
+if a in b:
+   print("Consonant")
+elif(a in c):
+  print("Vowel")
+else:
+ print("invalid")
